5117d2cdf99403cd6437f50bc2410c367f916464
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-14 16:11:56 +0300

    Проверить формулу

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -1,2 +1,13 @@
-﻿Console.Clear();
-
+﻿// Расстояние между точками в 3d
+Console.Clear();
+Console.WriteLine("Введите координаты 1-й точки: ");
+double x1=Convert.ToDouble(Console.ReadLine());
+double y1=Convert.ToDouble(Console.ReadLine());
+double z1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты 2-й точки: ");
+double x2=Convert.ToDouble(Console.ReadLine());
+double y2=Convert.ToDouble(Console.ReadLine());
+double z2=Convert.ToDouble(Console.ReadLine());
+double distance=Math.Sqrt(Math.Pow(x2+x1,2)+Math.Pow(y2+y1,2)+Math.Pow(z2+z1,2));
+distance=Math.Round(distance,2);
+Console.WriteLine($"Расстояние = {distance}");
