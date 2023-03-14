1c7f9103bd62fdd0ee2e0e92b79e4de7c8fc807d
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-14 15:14:35 +0300

    Task23 completed

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!2");
+﻿Console.Clear();
+Console.Write("Введите число: ");
+double number=Convert.ToDouble(Console.ReadLine());
+double numberCube;
+for (int index =1; index<=number;index++)
+{
+    numberCube=Math.Pow(index,3);
+    Console.WriteLine($"Число {index} в кубе = {numberCube}");
+}
