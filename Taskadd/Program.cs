42446fc8827cd8a6640d86a111376d368737d2e2
aleksey.krasnov <krasnov.80@mail.ru>
2023-03-16 12:24:28 +0300

    Кусты черники готово

---
+++ Taskadd/Program.cs
@@ -0,0 +1,18 @@
+﻿//Кусты черники
+Console.Clear();
+int bush =4;
+int [] berry=new int[bush];
+int sumBerry=0;
+int index=0;
+
+while(index<berry.Length)
+{
+    berry[index]=new Random().Next(1,10);
+    Console.Write(berry[index]);
+    index++;
+}
+sumBerry=berry[bush-1]+berry[bush-2]+berry[bush-3];
+
+Console.WriteLine();
+Console.Write(sumBerry);
+
